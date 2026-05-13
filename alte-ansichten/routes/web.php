c379f664625bbb71d36aa34d8c957a1a27fa62de
mkuess <100924943+mkuess@users.noreply.github.com>
2026-05-13 19:22:24 +0000

    Redirect / to admin, Karte improvements, Municipality media fix

--- alte-ansichten/routes/web.php
+++ alte-ansichten/routes/web.php
@@ -1,27 +1,9 @@
 <?php
-
-use App\Http\Controllers\MediaUploadController;
 use App\Http\Controllers\QrRedirectController;
-use App\Models\Municipality;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/qr/{code}', [QrRedirectController::class, 'redirect'])->name('qr.redirect');
 
-Route::middleware(['auth'])->group(function () {
-    Route::post('/admin/media-upload', [MediaUploadController::class, 'store'])->name('admin.media-upload');
-});
-
-
 Route::get('/', function () {
-    return view('public.home');
-});
-
-Route::get('/gemeinden', function () {
-    $municipalities = Municipality::with('district')
-        ->where('public_profile_enabled', true)
-        ->whereNotIn('status', ['hidden', 'archived'])
-        ->orderBy('name')
-        ->get();
-
-    return view('public.municipalities.index', compact('municipalities'));
+    return redirect('/admin');
 });
